346e0a26bf978b40fddb5ada29dd40a9f8f366a5
kuzy53 <nikita129343@mail.ru>
2024-01-19 02:13:56 +0300

    Updates

---
+++ precache-manifest.9f5036b2145928d1193f02abf1771f21.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fb59e0aac5202533901eb7ec6bc2e8",
+    "url": "/Car-Repair/index.html"
+  },
+  {
+    "revision": "8e3d8323c99478ac354b",
+    "url": "/Car-Repair/static/css/main.c376dd2e.chunk.css"
+  },
+  {
+    "revision": "a837fa522300e75510e3",
+    "url": "/Car-Repair/static/js/2.d48bc89f.chunk.js"
+  },
+  {
+    "revision": "8e3d8323c99478ac354b",
+    "url": "/Car-Repair/static/js/main.83f9ad18.chunk.js"
+  },
+  {
+    "revision": "f87a1a432e83255592b4",
+    "url": "/Car-Repair/static/js/runtime~main.d96f2cbc.js"
+  },
+  {
+    "revision": "2a4e26f2b90723c251ea05b5ae7519ab",
+    "url": "/Car-Repair/static/media/Add-User.2a4e26f2.svg"
+  },
+  {
+    "revision": "0d2f0e8a4bf95d7fef62cfb84dc7fde3",
+    "url": "/Car-Repair/static/media/Call.0d2f0e8a.svg"
+  },
+  {
+    "revision": "89ecffb116289cf403d04bfcd5bc50a4",
+    "url": "/Car-Repair/static/media/back.89ecffb1.png"
+  },
+  {
+    "revision": "aaedec65ed7f39d0a2822278807a1abc",
+    "url": "/Car-Repair/static/media/img.aaedec65.png"
+  },
+  {
+    "revision": "b0d177480601e516dedb054f7ba6ba45",
+    "url": "/Car-Repair/static/media/img10.b0d17748.png"
+  },
+  {
+    "revision": "0afe3b346450c36d279574420bdad6b5",
+    "url": "/Car-Repair/static/media/img2.0afe3b34.png"
+  },
+  {
+    "revision": "8d0fc43909e4bb977459f0007a077185",
+    "url": "/Car-Repair/static/media/img3.8d0fc439.png"
+  },
+  {
+    "revision": "c932331ced4e6cb838e111d9d060aaff",
+    "url": "/Car-Repair/static/media/img4.c932331c.png"
+  },
+  {
+    "revision": "edaacc174053ebf879ff485ece9236d7",
+    "url": "/Car-Repair/static/media/img5.edaacc17.png"
+  },
+  {
+    "revision": "05bdb4df0cf26cdefb1b8879579ae8ba",
+    "url": "/Car-Repair/static/media/img6.05bdb4df.png"
+  },
+  {
+    "revision": "017a36dc71fa387da08c667637d056c0",
+    "url": "/Car-Repair/static/media/star.017a36dc.svg"
+  }
+]);
